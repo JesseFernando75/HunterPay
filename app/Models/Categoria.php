d76a0a9dcdf79908e67a500cccc421ffa84671da
Isadora <isadora.ms2001@aluno.ifsc.edu.br>
2021-06-17 22:02:27 -0300

    Execução da tarefa 105

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cliente;
 
 class Categoria extends Model
 {
     use HasFactory;
 
     protected $table = 'categoria';
+
+    function cliente(){
+        	return $this->hasMany(Cliente::class, 'id_categoria', 'id');
+        }
 }
--- resources/views/cliente/editarcliente.blade.php
+++ resources/views/cliente/editarcliente.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.template')
 
 	@section('scripts')
-	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 	@endsection
@@ -32,6 +32,21 @@
 					<div class="col-md-12 mb-4">
 					    <label class="form-label">Número da conta</label>
 					    <input type="number" name="num_conta" class="form-control" placeholder="67807" value="{{ $v->num_conta }}" required>
+					</div>
+
+					<div class="col-md-6 mb-3">
+						<label for="f_email">Email</label>
+					   <input type="email" class="form-control" id="f_email" placeholder="name@example.com" name="email" value="{{ $v->email }}"required>
+					</div>
+					
+					<div class="col-md-6 mb-3">
+						<label for="f_senha">Senha</label>
+					 <input type="password" class="form-control" id="f_senha" placeholder="Senha" name="senha" value="{{ $v->senha }}"required>
+					</div>
+
+					<div class="col-md-6 mb-3">
+					  <label for="floatingInput">Cidade:</label>
+					  <input type="text" class="form-control"  placeholder="Cidade"  name="cidade" id="cidade" value="{{ $v->cidade }}"required>
 					</div>
 
 					<div class="col-md-12 d-grid gap-1 mb-5">
